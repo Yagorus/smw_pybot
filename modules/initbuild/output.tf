ac32ae540fd8f4d0507d6711d9cf53a5732b1ae7
Yegor <yegor.sidenko@gmail.com>
2022-12-09 08:15:44 +0200

    asd

--- modules/initbuild/output.tf
+++ modules/initbuild/output.tf
@@ -1,3 +1,11 @@
 output "ssm_db_pass_name" {
   value = aws_ssm_parameter.db_pass.name
 }
+
+
+output "aws_ssm_parameter" {
+  value = aws_ssm_parameter.key.value
+}
+output "aws_ssm_parameter" {
+  value = aws_ssm_parameter.token.value
+}
