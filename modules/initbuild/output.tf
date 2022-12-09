cbe2db25787cb6a38902cc7e3b31319e11dabf1b
Yegor <yegor.sidenko@gmail.com>
2022-12-09 08:17:47 +0200

    asd

--- modules/initbuild/output.tf
+++ modules/initbuild/output.tf
@@ -2,10 +2,9 @@ output "ssm_db_pass_name" {
   value = aws_ssm_parameter.db_pass.name
 }
 
-
-output "aws_ssm_parameter" {
+output "ssm_parameter" {
   value = aws_ssm_parameter.key.value
 }
-output "aws_ssm_parameter" {
+output "ssm_parameter" {
   value = aws_ssm_parameter.token.value
 }
